6c5703e60c5451d315e2b117f1dc0b732facca43
Myersjac <Myersjac@umich.edu>
2017-12-01 21:54:37 -0500

    blocks screen added in, guy moves now

--- scripts/page.js
+++ scripts/page.js
@@ -12,7 +12,7 @@ class coordinate_pair {
 	};
 };
 
-class smurf { 
+class smurf {
 	 constructor(){
 	 	this.coordinates = new coordinate_pair();
 	 };
@@ -53,13 +53,16 @@ function keydownRouter(e) {
     // If you would like to add arrow button features
     case KEYS.left:
 		add_to_movement_cache(smurf.coordinates.x_coord - 1, smurf.coordinates.y_coord);
+		break;
     case KEYS.right:
 		add_to_movement_cache(smurf.coordinates.x_coord + 1, smurf.coordinates.y_coord);
+		break;
     case KEYS.up:
-		add_to_movement_cache(smurf.coordinates.x_coord, smurf.coordinates.y_coord + 1);
-    case KEYS.down:
 		add_to_movement_cache(smurf.coordinates.x_coord, smurf.coordinates.y_coord - 1);
-      break;
+		break;
+    case KEYS.down:
+		add_to_movement_cache(smurf.coordinates.x_coord, smurf.coordinates.y_coord + 1);
+      	break;
     default:
       console.log("Invalid input!");
   }
@@ -73,19 +76,53 @@ $(document).ready( function() {
 	smurf = new smurf();
 	smurf.coordinates.x_coord = 7;
 	smurf.coordinates.y_coord = 0;
-	$('#smurf').css("top", smurf.coordinates.y_coord*75 );
-	$('#smurf').css("left", smurf.coordinates.x_coord*75 );
+	$('#smurf').css("top", smurf.coordinates.y_coord*30 );
+	$('#smurf').css("left", smurf.coordinates.x_coord*30 );
 
 
 
 	//game begins
 	// $("#start-button").click({
 		$(window).keydown(keydownRouter);
-		 setInterval( function() { 
+		 setInterval( function() {
 			actuate_movement_cache();
 		 }, 500);
 		
 	// });
+    
+
+    var block = ""
+
+    for ( var row = 0; row < 17; row++) {
+		for( var col = 0 ; col < 16 ; col++ ){
+		
+			block = "<img class='blocks' style='left:" + col*30 + ";top:" + row*30 + "' src='";
+			
+			if (row <=3) { //yellow
+				block += "img/ground_yellow.png" + "'>";
+			}
+			//middle tier
+			else if (row - 3 <= 3) { //orange
+			block += "img/ground_orange.png" + "'>";
+
+
+			}
+			//bottom tier
+			else { //red
+			block += "img/ground_red.png" + "'>";
+
+			}
+		
+			console.log(row);
+			console.log(col);
+			console.log(block);
+			$("#game-screen").append(block);
+		} //col
+		//append to div
+		console.log("HERE!");
+		console.log(row);
+		console.log(col);
+	} //row
 
 });
 
@@ -106,7 +143,7 @@ function alien_ai_move() {
 //ensures smurfs can't move faster than enemies
 //called at an interval
 //only fills up the movement cache so much otherwise there will be lag
-function smurf_move(next_movement) { 
+function smurf_move(next_movement) {
 	
 	console.log(next_movement);
 
@@ -114,10 +151,17 @@ function smurf_move(next_movement) {
 	let y = next_movement.y_coord;
 	
 	//check if space is clear
+	console.log("old top: " + $('#smurf').css("top"));
+	console.log("old left: " + $('#smurf').css("left"));
 	
 	//move smurf to new position
-	$('#smurf').css("top", y*75 );
-	$('#smurf').css("left", x*75 );
+	$('#smurf').css("top", y * 30 );
+	$('#smurf').css("left", x * 30 );
+
+	console.log("new top: " + $('#smurf').css("top"));
+	console.log("new left: " + $('#smurf').css("left"));
+
+
 	smurf.coordinates.x_coord = x;
 	smurf.coordinates.y_coord = y;
 
@@ -136,7 +180,7 @@ function actuate_movement_cache() {
 		return;
 	}
 	else { 
-		smurf_move(movement_cache[movement_cache_size]);
+		smurf_move(movement_cache[movement_cache_size - 1]);
 		--movement_cache_size; //performed movement
 	}
 }
@@ -148,6 +192,12 @@ function add_to_movement_cache(x, y) {
 	tmp.y_coord = y;
 	
 	//check if valid movement!
+
+	if(x<0 || y<0){
+		tmp.x_coord = 0;
+		tmp.y_coord = 0;
+		 //don't want negative values AKA him going off screen
+	}
 	
 	//don't make larger or will lag
 	if (movement_cache_size == 5) { 
